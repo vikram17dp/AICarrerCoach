02ce1cf28ebebd42db4e594e412d905363284bca
vikram17dp <vikramdp505@gmail.com>
2025-03-21 00:32:10 +0530

    completion of authentication

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
-
+import { dark } from "@clerk/themes";
 export const inter = Inter({
   subsets:["latin"]
 })
@@ -15,7 +15,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+    appearance={{
+      baseTheme:dark
+    }}
+    >
 
     <html lang="en" suppressHydrationWarning>
       <body
