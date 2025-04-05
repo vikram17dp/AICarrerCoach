2ae3b3f9a8bcb592f0c1b9909be95e6de13d359e
vikram17dp <vikramdp505@gmail.com>
2025-04-05 23:49:24 +0530

    working on hero page

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import HeroSection from "@/components/hero";
+import { Button } from "@/components/ui/button";
+
+export default function page() {
+  return (
+    <div className="">
+      <div className="grid-background">
+        <HeroSection/>
+      </div>
+    </div>
+  )
+}
+
+
---
+++ components/hero.jsx
@@ -0,0 +1,73 @@
+"use client";
+import Link from "next/link";
+import React, { useEffect, useRef } from "react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const HeroSection = () => {
+  const imageRef = useRef(null);
+
+
+  useEffect(() => {
+    const imageElement = imageRef.current;
+    console.log('Image ref:', imageRef.current);
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  
+  return (
+    <section className="w-full pt-36 md:pt-48 pb-10">
+      <div className="space-y-6 text-center">
+        <div className="space-y-6 mx-auto">
+          <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title animate-gradient">
+            Your AI Career Coach for
+            <br />
+            Professional Success
+          </h1>
+          <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl px-2">
+            Advance your career with personalized guidance, interview prep, and
+            AI-powered tools for job success.
+          </p>
+        </div>
+        <div className=" flex justify-center space-x-4  sm:py-2">
+          <Link href="/dashboard">
+            <Button size="lg" className="px-8 cursor-pointer">
+              Get Started
+            </Button>
+          </Link>
+          <Link href="https://www.youtube.com/roadsidecoder">
+            <Button size="lg" variant="outline" className="px-8 cursor-pointer">
+              Watch Demo
+            </Button>
+          </Link>
+        </div>
+        <div className="hero-image-wrapper mt-5 md:mt-0">
+          <div ref={imageRef} className="hero-image">
+            <Image
+              src="/banner2.jpeg"
+              width={1280}
+              height={720}
+              alt="Dashboard Preview"
+              className="rounded-lg shadow-2xl border mx-auto"
+              priority
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
